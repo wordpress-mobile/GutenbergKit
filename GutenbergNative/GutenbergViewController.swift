703ee0d9bf2171d20983c1d6c6d1daddf826b371
kean <grebenyuk.alexander@gmail.com>
2024-05-08 10:17:25 -0400

    Add initial Editor component version

--- GutenbergNative/GutenbergViewController.swift
+++ GutenbergNative/GutenbergViewController.swift
@@ -2,11 +2,20 @@ import UIKit
 import SwiftUI
 import WebKit
 
-final class GutenbergViewController: UIViewController {
+final class GutenbergViewController: UIViewController, WKNavigationDelegate{
     private var reactAppURL: URL!
+    private var webView: WKWebView!
+
+
+    let buttonOpenEditor = UIButton(type: .system)
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        buttonOpenEditor.setTitle("Launch Editor", for: [])
+        
+
+        reactAppURL = Bundle.main.url(forResource: "index", withExtension: "html", subdirectory: "build")
 
         // configuring the `WKWebView` is very important
         // without doing this the local index.html will not be able to read
@@ -18,9 +27,8 @@ final class GutenbergViewController: UIViewController {
         // don't worry about the frame: .zero, SwiftUI will resize the `WKWebView` to
         // fit the parent
         let webView = WKWebView(frame: .zero, configuration: config)
-        // now load the local url
-        webView.loadFileURL(reactAppURL, allowingReadAccessTo: reactAppURL)
-
+        webView.navigationDelegate = self
+        self.webView = webView
 
         view.addSubview(webView)
         webView.translatesAutoresizingMaskIntoConstraints = false
@@ -30,5 +38,30 @@ final class GutenbergViewController: UIViewController {
             webView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             webView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
         ])
+
+        view.addSubview(buttonOpenEditor)
+        buttonOpenEditor.addTarget(self, action: #selector(buttonEditorTapped), for: .touchUpInside)
+        buttonOpenEditor.tintColor = .systemBlue
+
+    }
+
+    @objc private func buttonEditorTapped() {
+        webView.loadFileURL(reactAppURL, allowingReadAccessTo: Bundle.main.resourceURL!)
+        buttonOpenEditor.isHidden = true
+    }
+
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+
+        buttonOpenEditor.sizeToFit()
+        buttonOpenEditor.center = CGPoint(x: view.bounds.width / 2, y: view.bounds.height / 2)
+    }
+
+    func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+        NSLog("didFailNavigation: \(error)")
+    }
+
+    func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+        NSLog("didFailProvisionalNavigation: \(error)")
     }
 }
