b6369830b363f6cedcb0cf4e99ca49600f4039dd
kean <grebenyuk.alexander@gmail.com>
2024-06-08 18:29:43 -0400

    Rebuild the dist

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,3 +1,3 @@
 (cd ./GutenbergReactApp && npm run build)
 rm -rf ./GutenbergKit/Sources/GutenbergKit/Gutenberg/
-cp -r ./GutenbergReactApp/build/. ./GutenbergKit/Sources/GutenbergKit/Gutenberg/
+cp -r ./GutenbergReactApp/dist/. ./GutenbergKit/Sources/GutenbergKit/Gutenberg/
