e3bb28a7a0195930bbd16685a4f4eb6527ecbe70
kean <grebenyuk.alexander@gmail.com>
2024-06-23 10:31:07 -0400

    Add BlockInserter

--- GutenbergReactApp/src/components/EditorToolbar.jsx
+++ GutenbergReactApp/src/components/EditorToolbar.jsx
@@ -1,9 +1,10 @@
-import { BlockToolbar } from '@wordpress/block-editor/build/components'
+import { BlockToolbar, Inserter } from '@wordpress/block-editor/build/components'
 import React from 'react'
 
 const EditorToolbar = () => {
   return (
     <div className='gbkit-editor-toolbar'>
+        <Inserter />
         <BlockToolbar />
     </div>
   )
