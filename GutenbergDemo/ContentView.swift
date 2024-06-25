3aa8ad2f23beca998e882de8e9dc2112db5f45a1
kean <grebenyuk.alexander@gmail.com>
2024-06-25 07:43:45 -0400

    Update editor demo

--- GutenbergDemo/ContentView.swift
+++ GutenbergDemo/ContentView.swift
@@ -3,7 +3,26 @@ import GutenbergKit
 
 struct ContentView: View {
     var body: some View {
-        EditorView()
+        NavigationView {
+            EditorView()
+        }
+        .navigationTitle("Editor Demo")
+    }
+}
+
+struct EditorView: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> EditorViewController {
+        EditorViewController(service: .init(client: Client()))
+    }
+
+    func updateUIViewController(_ uiViewController: EditorViewController, context: Context) {
+        // Do nothing
+    }
+}
+
+struct Client: EditorNetworkingClient {
+    func send(_ request: EditorNetworkRequest) async throws -> EditorNetworkResponse {
+        throw URLError(.unknown)
     }
 }
 
