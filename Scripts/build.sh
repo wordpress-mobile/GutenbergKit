20e8be23c32168d26f2b753014155e05f61654ee
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2024-07-16 17:42:25 -0600

    Re-arrange the projects

--- Scripts/build.sh
+++ Scripts/build.sh
@@ -1,4 +1,4 @@
 (cd ./ReactApp && npm run build)
-rm -rf ./Sources/GutenbergKit/Gutenberg/ ./app/src/main/assets
+rm -rf ./Sources/GutenbergKit/Gutenberg/ ./Demo-Android/app/src/main/assets/
 cp -r ./ReactApp/dist/. ./Sources/GutenbergKit/Gutenberg/
-cp -r ./ReactApp/dist/. ./app/src/main/assets/
+cp -r ./ReactApp/dist/. ./Demo-Android/app/src/main/assets/
