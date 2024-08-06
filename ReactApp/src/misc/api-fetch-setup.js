eace2de643289a53ac1c46bf16b88a45957bff98
Gerardo <gerardo.pacheco@automattic.com>
2024-08-06 16:47:30 +0200

    Add: API Fetch - Initial implementation setup

---
+++ ReactApp/src/misc/api-fetch-setup.js
@@ -0,0 +1,100 @@
+/**
+ * WordPress dependencies
+ */
+import apiFetch from '@wordpress/api-fetch';
+
+apiFetch.use(apiFetch.createRootURLMiddleware(''));
+
+apiFetch.use((options, next) => {
+	options.headers = options.headers || new Headers();
+	options.headers.set('Content-Type', 'application/json');
+
+	return next({
+		...options,
+		headers: {
+			Authorization: 'Basic ',
+		},
+	});
+});
+
+// Preload some endpoints to return data needed for some components
+// Like PostTitle.
+const LOCAL_POST_ID = 1;
+const LOCAL_AUTHOR_ID = 1;
+
+apiFetch.use(
+	apiFetch.createPreloadingMiddleware({
+		'/wp/v2/types?context=view': {
+			body: {
+				post: {
+					description: '',
+					hierarchical: false,
+					has_archive: false,
+					name: 'Posts',
+					slug: 'post',
+					taxonomies: ['category', 'post_tag'],
+					rest_base: 'posts',
+					rest_namespace: 'wp/v2',
+					template: [],
+					template_lock: false,
+					_links: {},
+				},
+				page: {
+					description: '',
+					hierarchical: true,
+					has_archive: false,
+					name: 'Pages',
+					slug: 'page',
+					taxonomies: [],
+					rest_base: 'pages',
+					rest_namespace: 'wp/v2',
+					template: [],
+					template_lock: false,
+					_links: {},
+				},
+			},
+		},
+		'/wp/v2/types/post?context=edit': {
+			body: {
+				name: 'Posts',
+				slug: 'post',
+				supports: {
+					title: true,
+					editor: true,
+					author: true,
+					thumbnail: true,
+					excerpt: true,
+					trackbacks: true,
+					'custom-fields': true,
+					comments: true,
+					revisions: true,
+					'post-formats': true,
+					autosave: true,
+				},
+				taxonomies: ['category', 'post_tag'],
+				rest_base: 'posts',
+				rest_namespace: 'wp/v2',
+				template: [],
+				template_lock: false,
+			},
+		},
+		[`/wp/v2/posts/${LOCAL_POST_ID}?context=edit`]: {
+			body: {
+				id: LOCAL_POST_ID,
+				slug: '',
+				status: 'auto-draft',
+				type: 'post',
+				title: { raw: 'Auto Draft', rendered: 'Auto Draft' },
+				content: {
+					raw: '',
+					rendered: '',
+					protected: false,
+					block_version: 0,
+				},
+				excerpt: { raw: '', rendered: '', protected: false },
+				author: LOCAL_AUTHOR_ID,
+				featured_media: 0,
+			},
+		},
+	})
+);
