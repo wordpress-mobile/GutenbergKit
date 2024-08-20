fb964ca9d8b12d2181cc52056099d8da35080a46
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2024-08-20 10:53:16 -0600

    Use a Makefile instead of Scripts

--- Scripts/build.sh
+++
@@ -1,4 +0,0 @@
-(cd ./ReactApp && npm run build)
-rm -rf ./Sources/GutenbergKit/Gutenberg/ ./Demo-Android/app/src/main/assets/
-cp -r ./ReactApp/dist/. ./Sources/GutenbergKit/Gutenberg/
-cp -r ./ReactApp/dist/. ./Demo-Android/app/src/main/assets/
