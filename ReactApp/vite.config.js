1041087c403806ecdf58734cf638d27af4a9660d
David Calhoun <github@davidcalhoun.me>
2024-08-28 09:19:40 -0400

    refactor: Remove unnecessary Vite config
    The `*.asset.php` file is unused for this project; the `lib`
    configuration is irrelevant as this project is not a library, but an
    application.

--- ReactApp/vite.config.js
+++ ReactApp/vite.config.js
@@ -1,16 +1,7 @@
-import { createHash } from 'crypto';
+import fs from 'fs';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import {
-	defaultRequestToExternal,
-	defaultRequestToHandle,
-} from '@wordpress/dependency-extraction-webpack-plugin/lib/util.js';
-import json2php from 'json2php';
-
-import packageMeta from './package.json';
-
-const kebabToCamel = (str) =>
-	str.replace(/-(\w)/, (all, g1) => g1.toUpperCase());
+import { defaultRequestToExternal } from '@wordpress/dependency-extraction-webpack-plugin/lib/util.js';
 
 const externalMap = new Map();
 
@@ -27,55 +18,20 @@ function globalize(id) {
 	return globalName;
 }
 
-function assetize() {
-	return {
-		name: 'assetize',
-
-		generateBundle(options, bundle) {
-			Object.entries(bundle).forEach(([fileName, fileInfo]) => {
-				if (!fileInfo.isAsset) {
-					const { imports, code } = fileInfo;
-					const scriptMeta = {
-						dependencies: imports
-							.map(defaultRequestToHandle)
-							.filter((o) => o != null),
-						version: createHash('sha1')
-							.update(code)
-							.digest('hex')
-							.slice(0, 20),
-					};
-					this.emitFile({
-						type: 'asset',
-						fileName: fileInfo.fileName.replace(
-							/\.js$/,
-							'.asset.php'
-						),
-						source: `<?php return ${json2php(scriptMeta)};\n`,
-					});
-				}
-			});
-		},
-	};
-}
-
 // https://vitejs.dev/config/
 export default defineConfig({
 	base: '',
 	build: {
-		lib: {
-			entry: './src/index.js',
-			name: kebabToCamel(packageMeta.name),
-			formats: ['iife'],
-		},
 		rollupOptions: {
 			external: externalize,
 			output: {
+				format: 'iife',
 				globals: globalize,
-				entryFileNames: `assets/${packageMeta.name}.js`,
+				entryFileNames: `assets/[name].js`,
 				chunkFileNames: `assets/[name].js`,
 				assetFileNames: `assets/[name].[ext]`,
 			},
 		},
 	},
-	plugins: [react({ jsxRuntime: 'classic' }), assetize()],
+	plugins: [react({ jsxRuntime: 'classic' })],
 });
