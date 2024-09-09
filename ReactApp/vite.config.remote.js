1047e4cd7913eeff15642b14154d7fa2cd58c38a
David Calhoun <github@davidcalhoun.me>
2024-09-09 16:17:21 -0400

    fix: Reinstate base location configuration
    The default of `/` incorrectly sources from the root of the project,
    rather than the nested build directory.

--- ReactApp/vite.config.remote.js
+++ ReactApp/vite.config.remote.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import { defaultRequestToExternal } from '@wordpress/dependency-extraction-webpack-plugin/lib/util.js';
 
 export default defineConfig({
+	base: '',
 	build: {
 		rollupOptions: {
 			input: 'remote.html',
