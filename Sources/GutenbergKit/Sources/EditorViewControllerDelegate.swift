e37c6e290ae3a458841fdb4c56a86162e745e911
David Calhoun <github@davidcalhoun.me>
2024-11-11 14:04:38 -0500

    refactor: Change `MediaInfo` from `Decodable` to `Codable`
    Increase robustness.

--- Sources/GutenbergKit/Sources/EditorViewControllerDelegate.swift
+++ Sources/GutenbergKit/Sources/EditorViewControllerDelegate.swift
@@ -78,20 +78,20 @@ extension OpenMediaLibraryAction.MediaType {
     }
 }
 
-public struct MediaInfo: Encodable {
+public struct MediaInfo: Codable {
     public let id: Int32?
     public let url: String?
     public let type: String?
     public let title: String?
     public let caption: String?
     public let alt: String?
-    public let metadata: [String: Any]
+    public let metadata: [String: String]
 
     private enum CodingKeys: String, CodingKey {
-        case id, url, type, title, caption, alt
+        case id, url, type, title, caption, alt, metadata
     }
 
-    public init(id: Int32?, url: String?, type: String?, caption: String? = nil, title: String? = nil, alt: String? = nil, metadata: [String: Any] = [:]) {
+    public init(id: Int32?, url: String?, type: String?, caption: String? = nil, title: String? = nil, alt: String? = nil, metadata: [String: String] = [:]) {
         self.id = id
         self.url = url
         self.type = type
