3c2e0856074e46a2b40016bbbe0bbfeb2b12bad1
David Calhoun <github@davidcalhoun.me>
2024-11-18 22:24:17 -0500

    style: Lint files outside `src` directory

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,17 @@
 module.exports = {
 	root: true,
 	extends: ['plugin:@wordpress/eslint-plugin/recommended'],
-	env: { browser: true, es2020: true },
-	ignorePatterns: ['dist', '.eslintrc.cjs'],
-	parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-	settings: { react: { version: '18.2' } },
+	env: {
+		browser: true,
+		es2020: true,
+	},
+	ignorePatterns: ['android', 'dist', 'ios'],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
 	plugins: ['react-refresh'],
 	rules: {
-		'react/jsx-no-target-blank': 'off',
 		'react-refresh/only-export-components': [
 			'warn',
 			{ allowConstantExport: true },
