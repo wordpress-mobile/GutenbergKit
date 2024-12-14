894c739541136b6becf39e5388fd4e45b9c04f2d
David Calhoun <github@davidcalhoun.me>
2024-12-13 19:50:41 -0500

    style: Co-locate import statements
    Follow Gutenberg project practices.

--- src/components/editor.jsx
+++ src/components/editor.jsx
@@ -18,22 +18,18 @@ import {
 } from '@wordpress/editor';
 import { useDispatch, useSelect, subscribe } from '@wordpress/data';
 import { store as coreStore, useEntityBlockEditor } from '@wordpress/core-data';
-
 // Default styles that are needed for the editor.
 import '@wordpress/components/build-style/style.css';
 import '@wordpress/block-editor/build-style/default-editor-styles.css';
 import '@wordpress/block-editor/build-style/style.css';
-import '@wordpress/block-editor/build-style/content.css';
-import '@wordpress/editor/build-style/style.css';
-
 // Default styles that are needed for the core blocks.
 import '@wordpress/block-library/build-style/style.css';
 import '@wordpress/block-library/build-style/editor.css';
 import '@wordpress/block-library/build-style/theme.css';
-
-// Registers standard formatting options for RichText.
 import '@wordpress/format-library';
 import '@wordpress/format-library/build-style/style.css';
+import '@wordpress/block-editor/build-style/content.css';
+import '@wordpress/editor/build-style/style.css';
 
 /**
  * Internal dependencies
