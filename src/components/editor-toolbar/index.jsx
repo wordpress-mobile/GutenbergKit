07b020eee0acbb8f4e307b6fa7ad0217ae494c89
David Calhoun <github@davidcalhoun.me>
2024-12-18 09:50:51 -0500

    fix: Disable block toolbar drag handle
    This handle is irrelevant for a fixed-position toolbar.

--- src/components/editor-toolbar/index.jsx
+++ src/components/editor-toolbar/index.jsx
@@ -69,7 +69,7 @@ const EditorToolbar = ({ className }) => {
 					</ToolbarGroup>
 				)}
 
-				<BlockToolbar />
+				<BlockToolbar hideDragHandle />
 			</Toolbar>
 
 			{isBlockInspectorShown && (
