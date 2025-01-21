7ab31c53a8dd2f6a5ebc587d356da608657ecea6
David Calhoun <github@davidcalhoun.me>
2025-01-21 16:04:39 -0500

    feat: Add rudimentary error boundary
    Catch render errors with Gutenberg's default error boundary component.

--- src/components/editor/index.jsx
+++ src/components/editor/index.jsx
@@ -4,7 +4,7 @@
 import { useEntityBlockEditor } from '@wordpress/core-data';
 import { privateApis as blockEditorPrivateApis } from '@wordpress/block-editor';
 import { useSelect } from '@wordpress/data';
-import { store as editorStore } from '@wordpress/editor';
+import { store as editorStore, ErrorBoundary } from '@wordpress/editor';
 
 /**
  * Internal dependencies
@@ -64,33 +64,35 @@ export default function Editor({ post, children }) {
 	}, []);
 
 	return (
-		<div className="gutenberg-kit-editor-interface">
-			<EditorLoadNotice className="gutenberg-kit-editor-interface__load-notice" />
-			<BlockEditorProvider
-				value={postBlocks}
-				onInput={onInput}
-				onChange={onChange}
-				settings={settings}
-				useSubRegistry={false}
-			>
-				{mode === 'visual' && isRichEditingEnabled && (
-					<VisualEditor
-						useRootPaddingAwareAlignments={
-							useRootPaddingAwareAlignments
-						}
-					/>
-				)}
+		<ErrorBoundary>
+			<div className="gutenberg-kit-editor-interface">
+				<EditorLoadNotice className="gutenberg-kit-editor-interface__load-notice" />
+				<BlockEditorProvider
+					value={postBlocks}
+					onInput={onInput}
+					onChange={onChange}
+					settings={settings}
+					useSubRegistry={false}
+				>
+					{mode === 'visual' && isRichEditingEnabled && (
+						<VisualEditor
+							useRootPaddingAwareAlignments={
+								useRootPaddingAwareAlignments
+							}
+						/>
+					)}
 
-				{(mode === 'text' || !isRichEditingEnabled) && (
-					<TextEditor
-						// We should auto-focus the canvas (title) on load.
-						// eslint-disable-next-line jsx-a11y/no-autofocus
-						autoFocus={true}
-					/>
-				)}
+					{(mode === 'text' || !isRichEditingEnabled) && (
+						<TextEditor
+							// We should auto-focus the canvas (title) on load.
+							// eslint-disable-next-line jsx-a11y/no-autofocus
+							autoFocus={true}
+						/>
+					)}
 
-				{children}
-			</BlockEditorProvider>
-		</div>
+					{children}
+				</BlockEditorProvider>
+			</div>
+		</ErrorBoundary>
 	);
 }
