b47ad9fdec63501c08e3fe351446cfa44a741101
David Calhoun <github@davidcalhoun.me>
2025-02-05 17:17:55 -0500

    feat: Modalize the block inserter and inspector
    Improve accessibility by hiding content outside of the modals from
    screen readers.

---
+++ src/components/editor-toolbar/aria-helper.js
@@ -0,0 +1,75 @@
+/**
+ * This file was sourced from the Gutenberg project and converted from
+ * TypeScript to JavaScript.
+ *
+ * @see https://github.com/WordPress/gutenberg/blob/3f0a805c568f92622faf4b71b24eeb5f39b5bca8/packages/components/src/modal/aria-helper.ts
+ */
+
+const LIVE_REGION_ARIA_ROLES = new Set([
+	'alert',
+	'status',
+	'log',
+	'marquee',
+	'timer',
+]);
+
+const hiddenElementsByDepth = [];
+
+/**
+ * Hides all elements in the body element from screen-readers except
+ * the provided element and elements that should not be hidden from
+ * screen-readers.
+ *
+ * The reason we do this is because `aria-modal="true"` currently is bugged
+ * in Safari, and support is spotty in other browsers overall. In the future
+ * we should consider removing these helper functions in favor of
+ * `aria-modal="true"`.
+ *
+ * @param {Element} modalElement The element that should not be hidden.
+ */
+export function modalize(modalElement) {
+	const elements = Array.from(document.body.children);
+	const hiddenElements = [];
+	hiddenElementsByDepth.push(hiddenElements);
+	for (const element of elements) {
+		if (element === modalElement) {
+			continue;
+		}
+
+		if (elementShouldBeHidden(element)) {
+			element.setAttribute('aria-hidden', 'true');
+			hiddenElements.push(element);
+		}
+	}
+}
+/**
+ * Determines if the passed element should not be hidden from screen readers.
+ *
+ * @param {Element} element The element that should be checked.
+ *
+ * @return {boolean} Whether the element should not be hidden from screen-readers.
+ */
+export function elementShouldBeHidden(element) {
+	const role = element.getAttribute('role');
+	return !(
+		element.tagName === 'SCRIPT' ||
+		element.hasAttribute('hidden') ||
+		element.hasAttribute('aria-hidden') ||
+		element.hasAttribute('aria-live') ||
+		(role && LIVE_REGION_ARIA_ROLES.has(role))
+	);
+}
+
+/**
+ * Accessibly reveals the elements hidden by the latest modal.
+ */
+export function unmodalize() {
+	const hiddenElements = hiddenElementsByDepth.pop();
+	if (!hiddenElements) {
+		return;
+	}
+
+	for (const element of hiddenElements) {
+		element.removeAttribute('aria-hidden');
+	}
+}
