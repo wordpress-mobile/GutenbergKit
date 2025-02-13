b48b27096348f7ca3e92d8b5d2f130fd0722a8a8
David Calhoun <github@davidcalhoun.me>
2025-02-13 16:20:16 -0500

    docs: Document VisualEditor

--- src/components/visual-editor/index.jsx
+++ src/components/visual-editor/index.jsx
@@ -53,6 +53,7 @@ const alignCSS = `.is-root-container.alignwide { max-width: var(--wp--style--glo
  *
  * @param {Object}  props                               Component props.
  * @param {boolean} props.useRootPaddingAwareAlignments Apply root padding.
+ * @param {boolean} props.hideTitle                     Whether to hide the title input.
  *
  * @return {JSX.Element} The rendered Editor component.
  */
