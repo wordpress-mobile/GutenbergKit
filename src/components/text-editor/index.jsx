d4a0086ed9d3491cc87e30150d5e8fb49e558767
David Calhoun <github@davidcalhoun.me>
2025-02-13 16:19:16 -0500

    docs: Document TextEditor

--- src/components/text-editor/index.jsx
+++ src/components/text-editor/index.jsx
@@ -8,6 +8,14 @@ import { PostTitleRaw, PostTextEditor } from '@wordpress/editor';
  */
 import './style.scss';
 
+/**
+ * TextEditor component renders a text editor with an optional title.
+ *
+ * @param {Object}  props           Component props.
+ * @param {boolean} props.hideTitle Whether to hide the title input.
+ *
+ * @return {JSX.Element} The rendered text editor component.
+ */
 export default function TextEditor({ hideTitle }) {
 	return (
 		<div className="gutenberg-kit-text-editor">
