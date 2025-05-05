be3786b4ad065e2d1e45e63ddeeb7291e391e4fe
David Calhoun <github@davidcalhoun.me>
2025-05-05 14:37:07 -0400

    fix: Ensure foundational styles are available for early error UI
    If the editor fails early in the load process, these styles need to be
    present for presenting error message UI. Therefore, we hoist them to the
    entry file.

--- src/components/visual-editor/index.jsx
+++ src/components/visual-editor/index.jsx
@@ -16,17 +16,7 @@ import { Popover } from '@wordpress/components';
 import { store as editorStore, PostTitle } from '@wordpress/editor';
 import { useSelect } from '@wordpress/data';
 import { store as editPostStore } from '@wordpress/edit-post';
-// Default styles that are needed for the editor.
-import '@wordpress/components/build-style/style.css';
-import '@wordpress/block-editor/build-style/style.css';
-// Default styles that are needed for the core blocks.
-import '@wordpress/block-library/build-style/style.css';
-import '@wordpress/block-library/build-style/editor.css';
-import '@wordpress/block-library/build-style/theme.css';
 import '@wordpress/format-library';
-import '@wordpress/format-library/build-style/style.css';
-import '@wordpress/block-editor/build-style/content.css';
-import '@wordpress/editor/build-style/style.css';
 
 /**
  * Internal dependencies
--- src/remote.jsx
+++ src/remote.jsx
@@ -2,6 +2,16 @@
  * WordPress dependencies
  */
 import apiFetch from '@wordpress/api-fetch';
+// Default styles that are needed for the editor.
+import '@wordpress/components/build-style/style.css';
+import '@wordpress/block-editor/build-style/style.css';
+// Default styles that are needed for the core blocks.
+import '@wordpress/block-library/build-style/style.css';
+import '@wordpress/block-library/build-style/editor.css';
+import '@wordpress/block-library/build-style/theme.css';
+import '@wordpress/format-library/build-style/style.css';
+import '@wordpress/block-editor/build-style/content.css';
+import '@wordpress/editor/build-style/style.css';
 
 /**
  * Internal dependencies
