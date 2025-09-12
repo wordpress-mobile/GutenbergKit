6406b8cbb8fa1937dddd1974e0a689b8c90051f4
David Calhoun <github@davidcalhoun.me>
2025-09-11 21:26:55 -0400

    docs: Further document nuanced editor settings fall back logic

--- src/utils/editor-settings.js
+++ src/utils/editor-settings.js
@@ -8,10 +8,9 @@ import { select } from '@wordpress/data';
 /**
  * Returns a default editor settings object to use when a site cannot provide
  * its own editor settings due to lacking the necessary experimental REST API
- * endpoint provided by the Gutenberg plugin
+ * endpoint provided by the Gutenberg plugin.
  *
  * @see https://github.com/WordPress/gutenberg/blob/d97807b50f756126798a04a5ae94745aee19356d/lib/experimental/class-wp-rest-block-editor-settings-controller.php
- * @see https://github.com/WordPress/gutenberg/pull/64448
  *
  * @return {Object} The default editor settings object
  */
@@ -21,9 +20,27 @@ export function getDefaultEditorSettings() {
 	const defaultEditorGradients = settings?.gradients;
 
 	return {
+		/**
+		 * These styles are used if the "no theme styles" options is triggered or on
+		 * themes without their own editor styles.
+		 *
+		 * @see https://github.com/WordPress/wordpress-develop/blob/db9654e6d386c2c69401b9c77c6ce1ae7460a6e4/src/wp-includes/block-editor.php#L218C4-L218C23
+		 */
 		defaultEditorStyles: [ { css: defaultEditorStyles } ],
 		__experimentalFeatures: {
-			blocks: {}, // Avoid errors from blocks relying upon block settings
+			/**
+			 * Avoid errors from blocks relying upon block settings.
+			 *
+			 * @see https://github.com/WordPress/gutenberg/pull/64448
+			 */
+			blocks: {},
+			/**
+			 * Ensure themes lacking their own theme styles can customize blocks using
+			 * the default editor colors and gradients. Mirrors the approach taken by
+			 * the Gutenberg Mobile editor.
+			 *
+			 * @see https://github.com/WordPress/gutenberg/blob/d97807b50f756126798a04a5ae94745aee19356d/packages/block-editor/src/components/global-styles/use-global-styles-context.native.js#L410-L433
+			 */
 			color: {
 				text: true,
 				background: true,
