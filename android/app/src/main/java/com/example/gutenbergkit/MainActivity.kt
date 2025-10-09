8f30f2eff55b221c4d3e123982e001516f4b73d8
David Calhoun <github@davidcalhoun.me>
2025-10-09 11:04:02 -0400

    refactor: Remove unused imports

--- android/app/src/main/java/com/example/gutenbergkit/MainActivity.kt
+++ android/app/src/main/java/com/example/gutenbergkit/MainActivity.kt
@@ -7,8 +7,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
