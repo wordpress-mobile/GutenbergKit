8a53ad7444f9f54ad397c3aeaa3675b812569955
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2025-10-27 12:11:13 -0600

    Remove LogLevel comparison

--- ios/Sources/GutenbergKit/Sources/LogLevel.swift
+++ ios/Sources/GutenbergKit/Sources/LogLevel.swift
@@ -1,21 +1,8 @@
 import Foundation
 
-public enum LogLevel: String, Decodable, Sendable, Comparable {
+public enum LogLevel: String, Decodable, Sendable {
     case error
     case warn
     case info
     case debug
-
-    public static func < (lhs: LogLevel, rhs: LogLevel) -> Bool {
-        lhs.intValue < rhs.intValue
-    }
-
-    private var intValue: UInt8 {
-        switch self {
-        case .error: 4
-        case .warn: 3
-        case .info: 2
-        case .debug: 1
-        }
-    }
 }
