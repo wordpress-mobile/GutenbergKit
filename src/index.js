50f81257f9e0c4b535b494c2f492153b3b89d4af
David Calhoun <github@davidcalhoun.me>
2025-11-04 08:43:19 -0500

    feat: Expose jQuery for plugins
    Some third-party code expects this foundational library that is
    available in the WP Admin context.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 /**
+ * External dependencies
+ */
+import jquery from 'jquery';
+
+/**
  * Internal dependencies
  */
 import { initializeBundledEditor } from './utils/bundled-editor';
 import './index.scss';
 
+window.jQuery = jquery; // Expose jQuery for plugins
 initializeBundledEditor();
