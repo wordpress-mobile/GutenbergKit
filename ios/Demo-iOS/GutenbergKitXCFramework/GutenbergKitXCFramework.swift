e6efc5f5255402012da11e6867b1dfe2b57d3033
Gio Lodi <gio@mokacoding.com>
2025-11-21 16:13:43 +1100

    Add framework target wrapper around GutenbergKit

---
+++ ios/Demo-iOS/GutenbergKitXCFramework/GutenbergKitXCFramework.swift
@@ -0,0 +1,5 @@
+// Plain framework target wrapping the GutenbergKit Swift package
+// to allow archiving as XCFramework.
+//
+// FIXME: Module 'GutenbergKit' was not compiled with library evolution support; using it means binary compatibility for 'GutenbergKitXCFramework' can't be guaranteed
+@_exported import GutenbergKit
