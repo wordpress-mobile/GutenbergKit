a3276998e2f474f89cd26e196961387b48c94207
Gio Lodi <gio@mokacoding.com>
2025-12-01 10:01:43 +1100

    Hardcode binary target to test in WordPress

--- Package.swift
+++ Package.swift
@@ -22,21 +22,30 @@ let package = Package(
         .package(url: "https://github.com/exyte/SVGView.git", from: "1.0.6"),
     ],
     targets: [
-        .target(
+        // Hardcoded binary target to test distribution in a client.
+        //
+        .binaryTarget(
             name: "GutenbergKit",
-            dependencies: ["SwiftSoup", "SVGView"],
-            path: "ios/Sources/GutenbergKit",
-            exclude: [],
-            resources: [.copy("Gutenberg")]
-        ),
-        .testTarget(
-            name: "GutenbergKitTests",
-            dependencies: ["GutenbergKit"],
-            path: "ios/Tests",
-            exclude: [],
-            resources: [
-                .copy("GutenbergKitTests/Resources/manifest-test-case-1.json")
-            ]
-        ),
+            url: "https://a8c-apps-public-artifacts.s3.us-east-2.amazonaws.com/gutenberg-kit/xcframework/a4374acb25d8f31bc17e9664294ba392c6fb03cd51d39cc4681ba48694afccda/GutenbergKit.xcframework.zip",
+            checksum: "a4374acb25d8f31bc17e9664294ba392c6fb03cd51d39cc4681ba48694afccda"
+        )
+        // Temporarily disabled just so we can try the binary distribution in a client.
+        //
+        // .target(
+        //     name: "GutenbergKit",
+        //     dependencies: ["SwiftSoup", "SVGView"],
+        //     path: "ios/Sources/GutenbergKit",
+        //     exclude: [],
+        //     resources: [.copy("Gutenberg")]
+        // ),
+        // .testTarget(
+        //     name: "GutenbergKitTests",
+        //     dependencies: ["GutenbergKit"],
+        //     path: "ios/Tests",
+        //     exclude: [],
+        //     resources: [
+        //         .copy("GutenbergKitTests/Resources/manifest-test-case-1.json")
+        //     ]
+        // ),
     ]
 )
