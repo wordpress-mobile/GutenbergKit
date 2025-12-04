5f619c6d888b558171f710d12ea6309b38ea306d
David Calhoun <github@davidcalhoun.me>
2025-12-04 13:50:20 -0500

    build: Upgrade `@vitejs/plugin-react`
    The latest version of `@vitejs/plugin-react` is ESM-only. This is
    incompatible with `@wordpress/eslint-plugin`'s usage of
    `eslint-import-resolver-node`. Once `eslint-import-resolver-node` is
    upgraded or replaced with a more capable alternative, we can remove
    the `eslint-disable-line` comments.
    
    Resulting error:
    
    ```
    Unable to resolve path to module '@vitejs/plugin-react'  import/no-unresolved
    ```
    
    See: https://github.com/vitejs/vite-plugin-react/issues/691#issuecomment-3172143364

--- vitest.config.js
+++ vitest.config.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react'; // eslint-disable-line import/no-unresolved
 
 export default defineConfig( {
 	plugins: [ react() ],
