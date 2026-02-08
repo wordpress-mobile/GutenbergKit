61cec51c4afc9f124c7795fe5ba475871bf36b5d
David Calhoun <github@davidcalhoun.me>
2026-02-07 22:32:36 -0500

    style: Ban deprecated Playwright APIs in E2E tests
    Add no-restricted-syntax rules to discourage `page.$()`, `page.$$()`
    and `page.waitForTimeout()` in favor of the locator API. These rules
    are aligned with the upstream Gutenberg E2E ESLint configuration and
    help prevent flaky tests.

--- e2e/.eslintrc.cjs
+++ e2e/.eslintrc.cjs
@@ -6,5 +6,23 @@ module.exports = {
 		// The WordPress ESLint config enforces jsdoc on all functions, but
 		// test files don't benefit from it.
 		'jsdoc/require-jsdoc': 'off',
+		// Discourage deprecated Playwright APIs in favor of locators, aligned
+		// with the upstream Gutenberg ESLint configuration.
+		'no-restricted-syntax': [
+			'error',
+			{
+				selector: 'CallExpression[callee.property.name="$"]',
+				message: '`$` is discouraged, please use `locator` instead.',
+			},
+			{
+				selector: 'CallExpression[callee.property.name="$$"]',
+				message: '`$$` is discouraged, please use `locator` instead.',
+			},
+			{
+				selector:
+					'CallExpression[callee.object.name="page"][callee.property.name="waitForTimeout"]',
+				message: 'Prefer page.locator instead.',
+			},
+		],
 	},
 };
