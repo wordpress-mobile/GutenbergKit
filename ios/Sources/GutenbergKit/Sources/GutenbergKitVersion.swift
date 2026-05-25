94d01382f0030ca492f5dfe8c0f7777fdb939aec
Jeremy Massel <1123407+jkmassel@users.noreply.github.com>
2026-05-25 14:51:08 -0600

    chore(release): 0.17.0

--- ios/Sources/GutenbergKit/Sources/GutenbergKitVersion.swift
+++ ios/Sources/GutenbergKit/Sources/GutenbergKitVersion.swift
@@ -4,5 +4,5 @@
 /// GutenbergKit version information.
 public enum GutenbergKitVersion {
     /// The current version of GutenbergKit.
-    public static let version = "0.16.0"
+    public static let version = "0.17.0"
 }
